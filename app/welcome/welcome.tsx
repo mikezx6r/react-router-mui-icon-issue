e6516dff22416fa2ddb1f7bbe4347d1c07899ee8
Mike Wilkes <mikezx6r@gmail.com>
2024-12-03 06:34:44 -0500

    Add MUI Icons and demonstrate issue

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,9 +1,11 @@
-import {Container, Typography} from "@mui/material";
+import {Checkbox, Container, Typography} from "@mui/material";
+import {Check} from "@mui/icons-material";
 
 export function Welcome() {
   return (
     <Container>
       <Typography>Some text goes here</Typography>
+      <Check/>
     </Container>
   );
 }
